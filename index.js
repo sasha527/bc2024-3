c1ce666254669c5fda120745e1099f61922d9b17
Olexander bc2024-3 <forostiansasha13@gmail.com>
2024-10-24 03:14:33 +0300

    .

--- index.js
+++ index.js
@@ -3,37 +3,36 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .requiredOption('-i, --input <path>', 'Input file path') // обов'язковий параметр
-  .option('-o, --output <path>', 'Output file path') // необов'язковий параметр
-  .option('-d, --display', 'Display result in console') // необов'язковий параметр
+  .requiredOption('-i, --input <path>', 'Input file path')
+  .option('-o, --output <path>', 'Output file path')
+  .option('-d, --display', 'Display result in console')
   .parse(process.argv);
 
 const options = program.opts();
 
-// Перевірка наявності обов'язкового параметру -i (input)
+//перевірочка
 if (!options.input) {
   console.error('Please, specify input file');
   process.exit(1);
 }
 
 try {
-  // Читання вмісту файлу
+  //асинхронне читання і далі ми створюємо об'єкт джейсона в консту
   const data = fs.readFileSync(options.input, 'utf8');
   const jsonData = JSON.parse(data);
 
-  // Фільтрація даних
+  /* фільтр по моєму варіанту (з джейсона вибираються варіанти з цими значеннями і потім мапом вибираються лише знач. value і створюється масив з тільки цими значеннями)*/
   const filteredData = jsonData
     .filter(item => item.ku === "13" && item.value > 5)
     .map(item => item.value);
 
-  // Виведення результатів, якщо вказано параметр -d
+  
   if (options.display) {
     console.log('Filtered values:', filteredData.join('\n'));
   }
 
-  // Запис у файл, якщо вказано параметр -o
   if (options.output) {
-    fs.writeFileSync(options.output, filteredData.join('\n'), 'utf8');
+    fs.writeFileSync(options.output, filteredData.join('\n'), 'utf8');              //синхронно записуємо в масив ....... .join('\n') для об'єднання в один рядок з роздільником
     console.log(`Filtered values have been written to ${options.output}`);
   }
 } catch (err) {
