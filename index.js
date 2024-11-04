b0d729e0047b49e221b76b1d46f3b1001dac693f
Olexander bc2024-3 <forostiansasha13@gmail.com>
2024-11-04 09:50:34 +0200

    фінал

--- index.js
+++ index.js
@@ -3,36 +3,43 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .requiredOption('-i, --input <path>', 'Input file path')
+  .option('-i, --input <path>', 'Input file path')
   .option('-o, --output <path>', 'Output file path')
   .option('-d, --display', 'Display result in console')
   .parse(process.argv);
 
 const options = program.opts();
 
-//перевірочка
 if (!options.input) {
-  console.error('Please, specify input file');
+  console.log('Please, specify input file');
   process.exit(1);
 }
 
 try {
-  //асинхронне читання і далі ми створюємо об'єкт джейсона в консту
   const data = fs.readFileSync(options.input, 'utf8');
-  const jsonData = JSON.parse(data);
+  let jsonData;
 
-  /* фільтр по моєму варіанту (з джейсона вибираються варіанти з цими значеннями і потім мапом вибираються лише знач. value і створюється масив з тільки цими значеннями)*/
-  const filteredData = jsonData
-    .filter(item => item.ku === "13" && item.value > 5)
-    .map(item => item.value);
+  try {
+    jsonData = JSON.parse(data);
+  } catch (jsonError) {
+    console.error('Invalid JSON format in input file');
+    process.exit(1);
+  }
 
-  
+  const filteredData = [];
+  for (let i = 0; i < jsonData.length; i++) {
+    const item = jsonData[i];
+    if (item.ku === "13" && item.value > 5) {
+      filteredData.push(item.value);
+    }
+  }
+
   if (options.display) {
     console.log('Filtered values:', filteredData.join('\n'));
   }
 
   if (options.output) {
-    fs.writeFileSync(options.output, filteredData.join('\n'), 'utf8');              //синхронно записуємо в масив ....... .join('\n') для об'єднання в один рядок з роздільником
+    fs.writeFileSync(options.output, filteredData.join('\n'), 'utf8');
     console.log(`Filtered values have been written to ${options.output}`);
   }
 } catch (err) {
